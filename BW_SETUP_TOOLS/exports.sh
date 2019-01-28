27f0db7ed44e0b44c16952f5109a6521c88a04ce
Christian Suess <csuess@hotmail.co.uk>
2019-01-28 13:40:38 +0000

    added exports in separate file

---
+++ BW_SETUP_TOOLS/exports.sh
@@ -0,0 +1,7 @@
+export RADICAL_PILOT_DBURL="mongodb://franklinb:Dg6w*l08@ds133816.mlab.com:33816/frank01" #Using Franklins MongoDB
+export RADICAL_VERBOSE=DEBUG
+export RADICAL_PILOT_VERBOSE=DEBUG
+export RADICAL_PILOT_AGENT_VERBOSE=DEBUG
+export RMQ_HOSTNAME='two.radical-project.org'
+export RMQ_PORT=33211
+export RADICAL_ENMD_PROFILING=1
